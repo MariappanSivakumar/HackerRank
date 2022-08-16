07936bdde38efe5c051b98781a182ab8aa1942bd
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-08-16 13:04:33 +0530

    Create Mini_MaxSum.java

---
+++ Mini_MaxSum.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static void minimax(long num[])
+    {
+        long big=num[0],small=num[0];
+        for(int i=0;i<num.length;i++)
+        {
+            if(big<num[i])
+                big=num[i];
+            if(num[i]<small)
+                small=num[i];
+        }
+        System.out.println(small+" "+big);
+    }
+    public static void miniMaxSum(List<Integer> arr) {
+        long nums[]=new long[arr.size()];
+        int count=0;
+        while(count!=(arr.size()))
+        {
+            int zip=0;
+            for(int i:arr)
+            {
+                if(zip!=count)
+                    nums[count]+=i;
+                zip++;    
+            }
+            count++;       
+        }
+        minimax(nums);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
